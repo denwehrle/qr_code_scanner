562cf56537ea3daba3eb62e6e1fa0c94df190da3
Dennis Wehrle <denwehrle@gmail.com>
2020-05-03 19:44:33 +0200

    fixed overlay position

--- lib/qr_scanner_overlay_shape.dart
+++ lib/qr_scanner_overlay_shape.dart
@@ -85,10 +85,10 @@ class QrScannerOverlayShape extends ShapeBorder {
       ..color = borderColor
       ..style = PaintingStyle.fill
       ..blendMode = BlendMode.dstOut;
-
+    
     final cutOutRect = Rect.fromLTWH(
-      width / 2 - _cutOutSize / 2 + borderOffset,
-      height / 2 - _cutOutSize / 2 + borderOffset,
+      rect.left + width / 2 - _cutOutSize / 2 + borderOffset,
+      rect.top + height / 2 - _cutOutSize / 2 + borderOffset,
       _cutOutSize - borderOffset * 2,
       _cutOutSize - borderOffset * 2,
     );
